634c6508e60b6f09e3ce67b756530ba1dbb32e59
Russell Pate <russ@russpate.com>
2016-03-01 15:47:10 -0500

    adding some basic js structure

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
 apiUrl = "http://api.bandsintown.com/events/search?location=Charleston,SC&radius=20&format=json&app_id=bandade";
+
+$(document).ready(function() {
+});
+var appID = ("bandade");
+var bandAde = {
+  init: function(){
+
+  },
+  styling: function(){
+
+  },
+  events: function(){
+
+  },
+  getFromDom: function(){
+
+  },
+  addToDom: function(){
+
+  },
+  getData: function(url){
+    $.ajax({
+      url: url,
+      method: 'GET',
+      data: param,
+      success: function (response) {
+          // code goes here
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
+  },
+
+  submitSearch: function(){
+
+  }
+}; // bandAde Page Array
