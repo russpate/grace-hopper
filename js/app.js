05e5ca444276fd6e0548f3931b6e53d51dce4993
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-03-02 13:37:50 -0500

    created artist filter

--- js/app.js
+++ js/app.js
@@ -11,20 +11,36 @@ var bandade = {
   initStyling: function () {
   },
   initEvents: function () {
-    $('#bandadeform').on('submit',  function (event) {
-      event.preventDefault();
-      $('.search-result').removeClass('inactive');
-      var searchTerm = $("#bandade-search-input").val();
-      var url = bandade.mainURL + searchTerm.replace(" ","+") + '&per_page=100&format=json&app_id=bandade';
-      bandade.getLocationData(url);
-    });
+    $('#bandadeform').on('submit', bandade.locationSearch);
+    $('#artist').on('submit', bandade.filterArtist);
   },
+
+  locationSearch: function () {
+    event.preventDefault();
+    $('.search-result').removeClass('inactive');
+    bandade.getLocationData(bandade.createLocationURL());
+  },
+
+  createLocationURL: function () {
+    var searchTerm = $("#bandade-search-input").val();
+    var locationURL = bandade.mainURL + searchTerm.replace(" ","+") + '&per_page=100&format=json&app_id=bandade';
+    return locationURL;
+  },
+
+   filterArtist: function () {
+     event.preventDefault();
+      var artistSearch = $('#artist-input').val();
+      var finalLocation = bandade.createLocationURL();
+      var artistURL = finalLocation + "&artists[]=" + artistSearch.replace(" ","");
+      bandade.getArtistData(artistURL);
+  },
+
   addToDom: function (item) {
-    $('.search-result').html('');
+    $('.main').html('');
     _.each(item, function (el){
       console.log(el);
       var tmpl = _.template(templates.searchResultsTemplate);
-      $('.search-result').append(tmpl(el));
+      $('.main').append(tmpl(el));
     });
   },
   getLocationData: function (url) {
@@ -33,11 +49,21 @@ var bandade = {
       method: 'GET',
       dataType: 'jsonp',
       success: function (location) {
-        window.glob = location;
         bandade.addToDom(location, $('.main'));
       }
     });
-  }
+  },
+  getArtistData: function (url) {
+    $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'jsonp',
+      success: function (artist) {
+        window.glob = artist;
+        bandade.addToDom(artist, $('.main'));
+      }
+    });
+  },
 }; //end of bandade obj
 
 // apiUrl = "http://api.bandsintown.com/events/search?location=Charleston,SC&radius=20&format=json&app_id=bandade";
