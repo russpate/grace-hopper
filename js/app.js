29eb8939405514e57840a108fd1cf39800e98e8a
Russell Pate <russ@russpate.com>
2016-03-01 23:51:28 -0500

    templates now functioning, needed to append instead of html

--- js/app.js
+++ js/app.js
@@ -9,40 +9,22 @@ var bandade = {
     bandade.initEvents();
   },
   initStyling: function () {
-
   },
   initEvents: function () {
     $('#bandadeform').on('submit',  function (event) {
       event.preventDefault();
       var searchTerm = $("#bandade-search-input").val();
-      var url = bandade.mainURL + searchTerm.replace(" ","") + '&format=json&app_id=bandade';
+      var url = bandade.mainURL + searchTerm.replace(" ","+") + '&format=json&app_id=bandade';
       bandade.getLocationData(url);
     });
   },
-  //
-  // This is what I was attempting for the template but
-  // wasn't able to get it to work just yet.
-  //
-  // addToDom: function (data) {
-  //   var tmpl = _.template(templates.searchResultsTemplate);
-  //   $('.searchResult').html('');
-  //   _.each(item, function (el)
-  //   {$('.searchResult').append(tmpl(el));
-  //   });
-  // },
-  addToDom: function (location, $target) {
-    $target.html('');
-    var htmlInsert = "";
-    _.each(location, function(item, idx) {
-        htmlInsert += "<div class = info-container>";
-        htmlInsert += "<p>" + "Artist: "  + item.artists[0].name + "</p>";
-        htmlInsert += "<p>" + "Venue: "  + item.venue.name + "</p>";
-        htmlInsert += "<p>" + "Location: "  + item.venue.city + " " + item.venue.region + "</p>";
-        htmlInsert += "<p>" + "Purchase Tickets: "  + item.ticket_url + "</p>";
-        htmlInsert += "<p>" + "Time and Data: "  + item.datetime + "</p>";
-        htmlInsert += "</div>";
+  addToDom: function (item) {
+    $('.search-result').append('');
+    _.each(item, function (el){
+      console.log(el);
+      var tmpl = _.template(templates.searchResultsTemplate);
+      $('.search-result').append(tmpl(el));
     });
-    $target.append(htmlInsert);
   },
 
   getLocationData: function (url) {
@@ -59,6 +41,6 @@ var bandade = {
 
 
 
-}; //end of banade obj
+}; //end of bandade obj
 
 // apiUrl = "http://api.bandsintown.com/events/search?location=Charleston,SC&radius=20&format=json&app_id=bandade";
