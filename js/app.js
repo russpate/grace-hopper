8f7983e2742c37400fceab332a7e927d978c612c
Dina O'Connor <dinasuslova@gmail.com>
2016-03-02 23:37:00 -0500

    worked on scss files, map and three inputs

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
   bandade.init();
 });
-var bandadeArray = "";
+
 var bandade = {
   mainURL:'http://api.bandsintown.com/events/search?location=',
-  eventLocationStore: [],
   init: function () {
     bandade.initStyling();
     bandade.initEvents();
@@ -15,30 +14,12 @@ var bandade = {
   initEvents: function () {
     $('#bandadeform').on('submit', bandade.locationSearch);
     $('#artist').on('submit', bandade.filterArtist);
-    $('#venue').on('submit', bandade.filterVenue);
-    $('#radius').on('submit', bandade.filterRadius);
-    $('#back-button').on('click', bandade.backButton);
   },
-  mappedArray: function(arr){
-    return arr.map(function (el) {
-      return{
-        artists: el.artists[0].name,
-        datetime: moment.utc(el.datetime).format("MMM Do, YYYY"),
-        ticket_url: el.ticket_url,
-        venue: el.venue.name,
-        latitude: el.venue.latitude,
-        longitude: el.venue.longitude,
-        city: el.venue.city,
-        region: el.venue.region
-      };
-    });
-  },
+
   locationSearch: function () {
     event.preventDefault();
     $('.search-result').removeClass('inactive');
     bandade.getLocationData(bandade.createLocationURL());
-    $('.search-results-info').text("All shows for: " + '"' + $("#bandade-search-input").val() + '"');
-    $('#back-button').hide();
   },
 
   createLocationURL: function () {
@@ -47,98 +28,43 @@ var bandade = {
     return locationURL;
   },
 
+   filterArtist: function () {
+     event.preventDefault();
+      var artistSearch = $('#artist-input').val();
+      var finalLocation = bandade.createLocationURL();
+      var artistURL = finalLocation + "&artists[]=" + artistSearch.replace(" ","");
+      bandade.getArtistData(artistURL);
+  },
+
   addToDom: function (item) {
     $('.main').html('');
     _.each(item, function (el){
+      console.log(el);
       var tmpl = _.template(templates.searchResultsTemplate);
       $('.main').append(tmpl(el));
     });
   },
-
-  filterArtist: function () {
-     event.preventDefault();
-     var artistArray = bandadeArray.filter(function (el) {
-     return $("#artist-input").val() === el.artists[0].name;
-   });
-     bandade.addToDom (artistArray);
-     $('.search-results-info').show();
-     $('#back-button').show();
-     $('.search-results-info').text("Search Results for: " + '"' + $("#artist-input").val()+ '"' + " in " + $("#bandade-search-input").val());
-     $('#artist-input').val('');
-  },
-
-  filterVenue: function (){
-      event.preventDefault();
-      var venueArray = bandadeArray.filter(function (el) {
-      return $("#venue-input").val() === el.venue.name;
-    });
-      bandade.addToDom (venueArray);
-      $('.search-results-info').show();
-      $('#back-button').show();
-      $('.search-results-info').text("Search Results for: " + '"' + $("#venue-input").val()+ '"' + " in " + $("#bandade-search-input").val());
-      $('#venue-input').val('');
-  },
-
-  filterRadius: function (){
-    event.preventDefault();
-    var radiusSearch = $('#radius-input').val();
-    var finalLocation = bandade.createLocationURL();
-    var radiusURL = finalLocation + "&radius=" + radiusSearch.replace(" miles","");
-    bandade.getVenueData(radiusURL);
-    $('.search-results-info').show();
-    $('#back-button').show();
-    $('.search-results-info').text("Radius Search Results: " + '"' + $("#radius-input").val()+ '"');
-    $('#radius-input').val('');
-
-  },
-
-  backButton: function () {
-    event.preventDefault();
-    $('.search-results-info').hide();
-    bandade.locationSearch();
-  },
-
   getLocationData: function (url) {
     $.ajax({
       url: url,
       method: 'GET',
       dataType: 'jsonp',
       success: function (location) {
-        var coords = location.map(function(el) {
-         return { lat: el.venue.latitude, lng: el.venue.longitude, title: el.venue.name };
-       });
-       bandade.eventLocationStore = [];
-       bandade.eventLocationStore.push(coords);
-       bandade.eventLocationStore[0].slice(0,10).forEach(function(coord,idx) {
-
-         var marker = new google.maps.Marker({
-           position: coord,
-           map: map,
-           title: coord.title
-         });
-       }),
-       map.setOptions({center:bandade.eventLocationStore[0][0]})
-       // reset map.setOptions with a center,
-        window.glob = location;
-        bandadeArray = location; //creating array of all data
-        // bandade.addToDom(location, $('.main'));
-        bandade.addToDom(bandade.mappedArray(location));
+        bandade.addToDom(location, $('.main'));
       }
     });
   },
-
-  getVenueData: function (url) {
-   $.ajax({
-     url: url,
-     method: 'GET',
-     dataType: 'jsonp',
-     success: function (radius) {
-       bandade.addToDom(radius, $('.main'));
-     }
-   });
- },
-
-}; //end of bandade obj
-
+  getArtistData: function (url) {
+    $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'jsonp',
+      success: function (artist) {
+        window.glob = artist;
+        bandade.addToDom(artist, $('.main'));
+      }
+    });
+  },
+}; //end of bandade objc
 
 // apiUrl = "http://api.bandsintown.com/events/search?location=Charleston,SC&radius=20&format=json&app_id=bandade";
