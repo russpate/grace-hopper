dd6a78eed735fafd698b5a39898a04bae4e2fc99
Russell Pate <russ@russpate.com>
2016-05-01 09:47:30 -0400

    a small refactor, updated for mobile, new typography, splash is full view and added a fancy transition after search, and everything is just a little better looking

--- js/templates.js
+++ js/templates.js
@@ -2,7 +2,7 @@ var templates = {};
 
 templates.searchResultsTemplate = [
   "<div class='info-container'>",
-    "<div class='event-info-wrap col-md-8 col-xs-12'>",
+    "<div class='event-info-wrap col-md-7 col-sm-7 col-xs-12'>",
       "<div class='bandade-artist'>",
         "<%= artists %>",
       "</div>",
@@ -17,7 +17,7 @@ templates.searchResultsTemplate = [
         "<%= datetime %>",
       "</div>",
     "</div>",
-    "<div class='tix-wrap col-md-4 col-xs-12'>",
+    "<div class='tix-wrap col-md-5 col-sm-5 col-xs-12'>",
       "<div class='bandade-tix'><a href='",
         "<%= ticket_url %>",
       "'><button>Purchase Tickets</button></a></p>",
